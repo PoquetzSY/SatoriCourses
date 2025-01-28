2cfe716c3348f8cd339a5df91419aecfe61fc1d0
Rodrigo <rodrigo17.ed@gmail.com>
2025-01-28 09:39:16 -0600

    Login admin/user

---
+++ src/views/AdminDashboardView.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'AdminDashboardView',
+};
+</script>
+
+<template>
+  <div class="w-full flex items-center justify-center h-screen">
+    <h1 class="text-4xl font-bold text-red-600">Panel de Administración Prueba</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
